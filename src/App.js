244e5845c2990eb886d57297f7857dbc40cd75f5
LucianoSainz <danielsainz28@gmail.com>
2021-05-05 12:34:34 +0200

    regactoring

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import {useState} from 'react';
 export default function App(props) {
   const [notes, setNotes] = useState(props.notes)
   return(
+    <div>
     <ol>
       {notes.map((note) => (
         <Note key={note.id} {...note} />
       ))}
+      </ol>
+
       <div>
       <input type='text' />
-      <button>Crear nota</button>
+      <button>New Note</button>
       </div>
-    </ol>
-  );
+    </div>
+    );
 }
 
